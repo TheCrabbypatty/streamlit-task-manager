7738fe534474793444dae950d7a1a56c20c0764f
TheCrabbypatty <plchen@madison.k12.wi.us>
2026-04-18 13:17:08 +0000

    Fixed deleting events, added dates support and functionality.

--- manager/manager.py
+++ manager/manager.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import random 
 
 global checkbox_num
 if "checkbox_num" not in st.session_state:
@@ -15,6 +16,9 @@ global delete_list
 if "delete_list" not in st.session_state:
     st.session_state.delete_list = []
 
+global event_date
+if "event_date" not in st.session_state:
+    st.session_state.event_date = []
 
 def main(): 
     setup()
@@ -32,9 +36,12 @@ with col1:
     def button():
         add_event = st.button("Add event!", key = "add")
         events = st.text_input("Event name 👇", placeholder = "+ Add Task", key = "event").strip()
+        dates = st.date_input("Event date 👇", format = "MM/DD/YYYY", key = "date")
         if add_event and not len(st.session_state.event) == 0:
             with open("todo.txt", "a") as file:
                 file.write(f"{events}\n")
+            with open("date.txt", "a") as file:
+                file.write(f"{dates}\n")
             st.session_state.checkbox_num += 1
             st.toast("Your event has been added!", icon = "👍", duration = 1)
             
@@ -44,12 +51,17 @@ with col1:
         with open("todo.txt", "r") as file:
             st.session_state.event_title = file.readlines()
         with open("delete.txt", "r") as file:
-            st.session_state.delete_list = file.readlines()
-            st.session_state.delete_list = [int(x.strip()) for x in file.readlines() if x.strip()]
+            try:
+                st.session_state.delete_list = file.readlines()
+                st.session_state.delete_list = [int(item.strip()) for item in st.session_state.delete_list]
+            except ValueError:
+                pass
+        with open("date.txt", "r") as file:
+            st.session_state.event_date = file.readlines()
         for i in range(st.session_state.checkbox_num):
             try:
                 if i not in st.session_state.delete_list:
-                    st.checkbox(st.session_state.event_title[i], key = f"task_{i}", on_change = checked, args = (f"task_{i}", ))
+                    st.checkbox(f"{st.session_state.event_title[i]} :red-badge[{st.session_state.event_date[i]}]", key = f"task_{i}", on_change = checked, args = (f"task_{i}", ))
             except IndexError:
                 pass
             
@@ -64,6 +76,11 @@ with col1:
             lines[number] = f"\n"
         with open("todo.txt", "w") as file:
             file.writelines(lines)
+        with open("date.txt", "r") as file:
+            lines = file.readlines()
+            lines[number] = f"\n"
+        with open("date.txt", "w") as file:
+            file.writelines(lines)
 
 
 if __name__ == "__main__":
@@ -71,10 +88,4 @@ if __name__ == "__main__":
 
 
 ''' 
-What to do:
-1. ~Make it so that enter also clears the text box~
-2. Add maybe additional features, like editing a current item.
-3. Add additional information, like date, time, ect....
-4. If time, implment the CSV and txt's permenant memory.
-5. Cool, random features, like a timer?
 '''
